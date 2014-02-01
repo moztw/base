88a0489cc3755eb6ba1143d996d205bae52ccdf8
Peter Pin-Guang Chen <petercpg@mail.moztw.org>
2014-02-01 12:56:37 +0800

    Update update.sh for pulling central and aurora

--- conf/update.sh
+++ conf/update.sh
@@ -6,8 +6,7 @@ echo
 date
 echo
 
-if [ $USER != www-data ]
-then
+if [ $USER != www-data ]; then
 	echo 'You must run this script as www-data (try sudo -u www-data)'
 	exit
 fi
@@ -18,20 +17,17 @@ for REPO in `ls $ROOT -1`
 do
 	echo '* '$REPO
 	cd $ROOT/$REPO
-	if [ -e $ROOT/$REPO/.hg ]
-	then
+	if [ -e $ROOT/$REPO/.hg ]; then
 		# pull everything down then update to default
 		hg pull -u
 		hg update -c -r default
 		# show tip information
 		hg tip
 	fi
-	if [ -e $ROOT/$REPO/.svn ]
-	then
+	if [ -e $ROOT/$REPO/.svn ]; then
 		svn update
 	fi
-	if [ -e $ROOT/$REPO/.git ]
-	then
+	if [ -e $ROOT/$REPO/.git ]; then
 		git pull
 		git log -n 1
 		echo
@@ -39,16 +35,16 @@ do
 done
 
 #Importing mozilla-central and Gaia repositories to Narro
-for PRJID in 9 11 12 13 14 15
+for PRJID in 2 4 5 6 7 8 9 11 12 13 14 15
 do  
 	echo
-	echo 'Importing l10n-aurora projects...'
+	echo 'Importing l10n-central and l10n-aurora projects...'
 	/usr/bin/php /home/moztw/translate/www/narro/includes/narro/importer/narro-cli.php --import --minloglevel 3 --project $PRJID --user 1 --check-equal --import-unchanged-files --template-lang en-US --translation-lang zh-TW --template-directory /home/moztw/translate/www/narro/data/import/$PRJID/en-US --translation-directory /home/moztw/translate/www/narro/data/import/$PRJID/zh-TW
 	echo
 	echo 'Done.'
 done
 
-for PRJID in 26 30 31
+for PRJID in 26 30 31 32
   do
     echo
     echo 'Importing Gaia projects...'
