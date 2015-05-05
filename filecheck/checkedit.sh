cc34b819cb7a3fafd039630244607917edcf79d4
Peter Pin-Guang Chen <petercpg@mail.moztw.org>
2015-05-05 16:46:01 +0800

    Fix checkedit.sh contents

--- filecheck/checkedit.sh
+++ filecheck/checkedit.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-HOSTNAME=$(hostname)
+HOSTNAME=$(hostname -f)
 DATE=$(date +"%Y-%m-%d %T")
 LOGFILE=/tmp/file_log.log
 FCHECK=/usr/sbin/fcheck
+FCHECKCONF=/etc/fcheck/fcheck.cfg
 
 # Only let root run this script
 if [ $EUID -ne 0 ]; then
@@ -11,15 +12,15 @@ if [ $EUID -ne 0 ]; then
   exit 1
 fi
 
-$FCHECK -ar > $LOGFILE
-$FCHECK -arc > /dev/null
+$FCHECK -ar $FCHECKCONF > $LOGFILE
+$FCHECK -arc $FCHECKCONF > /dev/null
 
 FCHECKSTATUS=$(tail -n 2 $LOGFILE | head -n 1)
 if [ "$FCHECKSTATUS" = "STATUS:passed..." ]; then
-  echo "" | mail -s "($DATE) MozTW.org File Change Info - None EOM" admin@mail.moztw.org
+  echo "" | mail -s "($DATE) $HOSTNAME File Change Info - None EOM" admin@mail.moztw.org
   exit 1
 else
-  mail -s "($DATE) MozTW.org File Change Info" admin@mail.moztw.org < $LOGFILE
+  mail -s "($DATE) $HOSTNAME File Change Info" admin@mail.moztw.org < $LOGFILE
   rm -rf $LOGFILE
   exit 0
 fi
