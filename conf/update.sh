c4b16c24e1256df08eb0650973068db8159e4384
Peter Pin-Guang Chen <petercpg@mail.moztw.org>
2016-05-01 01:45:27 +0800

    remove gaia and add l10n-beta repo to the daily import script

--- conf/update.sh
+++ conf/update.sh
@@ -8,52 +8,42 @@ date
 echo
 
 if [ $USER != www-data ]; then
-	echo 'You must run this script as www-data (try sudo -u www-data)'
-	exit
+  echo 'You must run this script as www-data (try sudo -u www-data)'
+  exit
 fi
 
 echo 'Pulling English source code from upstream...'
 
 for REPO in `ls $ROOT -1`
 do
-	echo '* '$REPO
-	cd $ROOT/$REPO
-	if [ -e $ROOT/$REPO/.hg ]; then
-		# pull everything down then update to default
-		hg pull -u
-		hg update -c -r default
-		# show tip information
-		hg tip
-	fi
-	if [ -e $ROOT/$REPO/.svn ]; then
-		svn update
-	fi
-	if [ -e $ROOT/$REPO/.git ]; then
-		git pull
-		git log -n 1
-		echo
-	fi
+  echo '* '$REPO
+  cd $ROOT/$REPO
+  if [ -e $ROOT/$REPO/.hg ]; then
+    # pull everything down then update to default
+    hg pull -u
+    hg update -c -r default
+    # show tip information
+    hg tip
+  fi
+  if [ -e $ROOT/$REPO/.svn ]; then
+    svn update
+  fi
+  if [ -e $ROOT/$REPO/.git ]; then
+    git pull
+    git log -n 1
+    echo
+  fi
 done
 
 #Importing mozilla-central and Gaia repositories to Narro
-for PRJID in 2 4 5 6 7 8 9 11 12 13 14 15
+for PRJID in 2 4 5 6 7 8 9 11 12 13 14 15 16 18 19 20 21 23 
 do  
-	echo
-	echo "Importing l10n-central and l10n-aurora projects... PRJID $PRJID"
-	/usr/bin/php /home/moztw/translate/www/narro/includes/narro/importer/narro-cli.php --import --minloglevel 3 --project $PRJID --user 1 --check-equal --import-unchanged-files --template-lang en-US --translation-lang zh-TW --template-directory /home/moztw/translate/www/narro/data/import/$PRJID/en-US --translation-directory /home/moztw/translate/www/narro/data/import/$PRJID/zh-TW
-	echo
-	echo 'Done.'
+  echo
+  echo "Importing l10n-central and l10n-aurora projects... PRJID $PRJID"
+  /usr/bin/php /home/moztw/translate/www/narro/includes/narro/importer/narro-cli.php --import --minloglevel 3 --project $PRJID --user 1 --check-equal --import-unchanged-files --template-lang en-US --translation-lang zh-TW --template-directory /home/moztw/translate/www/narro/data/import/$PRJID/en-US --translation-directory /home/moztw/translate/www/narro/data/import/$PRJID/zh-TW
+  echo
+  echo 'Done.'
 done
-
-for PRJID in 26 34 35
-  do
-    echo
-    echo "Importing Gaia projects... PRJID $PRJID"
-    /usr/bin/php /home/moztw/translate/www/narro/includes/narro/importer/narro-cli.php --import --minloglevel 3 --project $PRJID --user 1 --check-equal --import-unchanged-files --template-lang en-US --translation-lang zh-TW --template-directory /home/moztw/translate/www/narro/data/import/$PRJID/en-US --translation-directory /home/moztw/translate/www/narro/data/import/$PRJID/zh-TW
-    echo
-    echo 'Done.'
-done
-
 
 echo
 date
